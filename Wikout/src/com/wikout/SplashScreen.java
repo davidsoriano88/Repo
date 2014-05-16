efcf25f1e795d63c982916e326e13eec9ecd9258
davidsoriano88 <davidsorianodc@hotmail.es>
2014-05-16 12:14:44 +0200

    Refactorizacion layouts 12:14

--- Wikout/src/com/wikout/SplashScreen.java
+++ Wikout/src/com/wikout/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends Activity {
   protected void onCreate(Bundle savedInstanceState) 
   {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_splash_screen);
     initUI();
   }
   
