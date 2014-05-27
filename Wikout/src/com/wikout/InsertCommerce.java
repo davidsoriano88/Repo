42a2b83ef1a5533ae61cdc40ef415e251cfef247
Turo <artajahuerce@gmail.com>
2014-05-27 12:49:15 +0200

    12.49

---
+++ Wikout/src/com/wikout/InsertCommerce.java
@@ -0,0 +1,306 @@
+package com.wikout;
+import io.backbeam.BackbeamException;
+import io.backbeam.BackbeamObject;
+import io.backbeam.FileUpload;
+import io.backbeam.Location;
+import io.backbeam.ObjectCallback;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import utils.CustomOnItemSelectedListener;
+import utils.Photo;
+import utils.Util;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class InsertCommerce extends Activity {
+	
+	Double latitude, longitude;
+	String position;
+	ImageView ivPhoto;
+	EditText etPlacename;
+	Button btnOk;
+	Spinner spnCategory;
+
+	// Otras variables
+	String photoPath, idoferta, idphoto = "";
+
+	// Variables para controlar la fecha
+
+	
+	// variables para control de fotografias
+	String photoName, url;
+	File photo;
+	public int existPhoto = 0;
+	
+	// constantes utilizadas para lanzar intents
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	static final int LOAD_IMAGE = 3;
+
+	// Location de prueba
+	public Location locationbm;
+
+	// Constante para el picker
+	
+	final Context context = this;
+	
+	Util util= new Util();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.insert_commerce);
+		util.projectData(context);
+		initUI();
+		
+	}
+	 private void initUI(){
+		
+		ivPhoto = (ImageView) findViewById(R.id.ivInsertPhoto1);
+		etPlacename = (EditText) findViewById(R.id.etInsertPlacename1);
+		btnOk = (Button) findViewById(R.id.btnInsertOk1);
+		addListenerOnSpinnerItemSelection();
+
+		btnOk.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+					if( etPlacename.getText().length()==0){
+						util.log("aceptar1");
+					dialogIncompleteFields();
+					}else if(existPhoto == 1){
+					
+						insertPhoto();
+						finish();
+					} else {
+						imageClicked(v);
+					}
+					
+				}
+				});
+		}
+	 
+
+
+	
+
+	public void dialogGetLocation() {
+		AlertDialog.Builder dialogLocation = new AlertDialog.Builder(this);
+		dialogLocation.setTitle("Ubicacion");
+		dialogLocation.setMessage("Elija la direccion:");
+		dialogLocation.setCancelable(true);
+		dialogLocation.setPositiveButton("Ubicacion actual",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogo1, int id) {
+						SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+						String myLatitude = prefs.getString("latpos", "no id");
+						latitude = Double.parseDouble(myLatitude);
+						String myLongitude = prefs.getString("longpos", "no id");
+						longitude = Double.parseDouble(myLongitude);
+						
+
+					}
+
+				});
+		dialogLocation.setNegativeButton("Indicar en el mapa",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogo1, int id) {
+						Bundle bundle = getIntent().getExtras();
+						latitude = bundle.getDouble("latiMain");
+						longitude = bundle.getDouble("longiMain");
+						finish();
+					}
+				});
+		dialogLocation.show();
+	}
+	public void dialogIncompleteFields() {
+		AlertDialog.Builder dialogIncomplete = new AlertDialog.Builder(this);
+		dialogIncomplete.setTitle("Información incompleta");
+		dialogIncomplete.setMessage("Rellene los campos Incompletos, por favor.");
+		dialogIncomplete.setCancelable(false);
+		dialogIncomplete.setNeutralButton("Aceptar",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogo1, int id) {
+						
+
+					}
+
+				});
+		
+		dialogIncomplete.show();
+	}
+	public void addListenerOnSpinnerItemSelection() {
+
+		spnCategory = (Spinner) findViewById(R.id.spnInsertCategory);
+		spnCategory.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+
+	}
+
+	
+	// METODO PARA SUBIR FOTO
+		private void insertPhoto() {
+			//Extraigo la fecha actual
+			Calendar calendar = new GregorianCalendar();
+			final Date createdate = calendar.getTime();
+			//Creo el objeto commerce
+			final BackbeamObject objectphoto = new BackbeamObject("file");
+			//Hay que pasarle el objeto de tipo file "foto"
+			objectphoto.uploadFile(new FileUpload(photo, "image/jpg"),
+					new ObjectCallback() {
+						@Override
+						public void success(BackbeamObject photo) {
+							System.out.println("success!! " + photo.getId());
+							photo.setString("idphoto", photo.getId());
+							photo.setDate("uploaddate", createdate);
+							photo.save(new ObjectCallback() {
+								@Override
+								public void success(BackbeamObject objetofoto) {
+									System.out.println("foto subida con éxito!! "
+											+ objetofoto.getId());
+									insertCommerce(objetofoto);
+								}
+							});
+						}
+
+						@Override
+						public void failure(BackbeamException exception) {
+							System.out.println("failure!");
+							exception.printStackTrace();
+						}
+					});
+		}
+		
+	// INSERTAR NUEVO "COMMERCE"
+		private void insertCommerce(BackbeamObject objectphoto) {
+			locationbm = new Location(latitude, longitude);
+			//Extraigo la fecha actual
+			Calendar calendar = new GregorianCalendar();
+			final Date createdate = calendar.getTime();
+			//Creo el objeto commerce
+			final BackbeamObject commerce = new BackbeamObject("commerce");
+			//Relleno los campos del objeto
+			commerce.setString("placename", etPlacename.getText().toString());
+			commerce.setLocation("placelocation", locationbm);
+			commerce.setString("category",(String) spnCategory.getSelectedItem() );
+			commerce.setDate("commercecreationdate", createdate);
+			commerce.setNumber("numbubble", 0);
+			commerce.setString("udid", getId());
+			commerce.setObject("file", objectphoto);
+			//Guardo el objeto
+			commerce.save(new ObjectCallback() {
+				@Override
+				public void success(BackbeamObject object) {
+					
+					//Llamo al metodo insertOffer para enlazarlo con la oferta
+					//insertOffer(createdate, object);****************************REVISAR
+
+				}
+			});
+
+		}
+
+
+	private String getId() {
+		String id = android.provider.Settings.System.getString(
+				super.getContentResolver(),
+				android.provider.Settings.Secure.ANDROID_ID);
+		return id;
+	}
+
+
+	public void imageClicked(View imageView) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setMessage(R.string.question)
+				.setPositiveButton(R.string.camera,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int id) {
+								onPhotoDialogPositiveClick();
+							}
+						})
+				.setNegativeButton(R.string.galeria,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int id) {
+								onPhotoDialogNegativeClick();
+							}
+						});
+
+		// launch dialog
+		AlertDialog alertDialog = builder.create();
+		alertDialog.show();
+
+	}
+
+	// launching camera activity
+	public void onPhotoDialogPositiveClick() {
+		// launch camera
+		Intent picture = new Intent(this, Photo.class);
+		picture.putExtra("ACTION_REQUESTED", "CAMERA");
+		startActivityForResult(picture, REQUEST_IMAGE_CAPTURE);
+	}
+
+	// if gallery has been pushed
+	public void onPhotoDialogNegativeClick() {
+		// launch gallery
+		Intent picture = new Intent(this, Photo.class);
+		picture.putExtra("ACTION_REQUESTED", "GALLERY");
+		startActivityForResult(picture, LOAD_IMAGE);
+	}
+
+	
+	// load image in imageView
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		
+		// if there's no errors, the image is loaded
+		if (resultCode == RESULT_OK) {
+
+			Bundle photoBundle = new Bundle();
+			util.log("bundle created");
+			photoBundle = data.getExtras();
+
+			photoPath = photoBundle.getString("path");
+			photoName = photoBundle.getString("photoName");
+			url = photoBundle.getString("url");
+			photo = (File) photoBundle.get("photo");
+
+			util.log(photoBundle.getString("photoName"));
+
+			Bitmap cphoto = BitmapFactory.decodeFile(photoPath);
+			ivPhoto.setImageBitmap(cphoto);
+			existPhoto = 1;
+
+		}
+	}
+
+}
+
--- Wikout/src/com/wikout/Mapv2.java
+++ Wikout/src/com/wikout/Mapv2.java
@@ -1,5 +1,193 @@
 package com.wikout;
 
-public class Mapv2 {
+import utils.Util;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
 
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+
+public class Mapv2 extends ActionBarActivity {
+	
+	Util util = new Util();
+	private GoogleMap map;
+	LocationManager locationManager;
+	Location location;
+	double longitudeSW, latitudeSW, longitudeNE, latitudeNE;
+	LocationClient mLocationClient;
+    Context context;  
+
+  
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	 super.onCreate(savedInstanceState);
+	 supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+	 setContentView(R.layout.fragment_mapv2);
+	 context=this;
+
+     util.projectData(context);
+     initUI();  
+ 	
+	 }
+
+	public void initUI(){
+		
+		setSupportProgressBarIndeterminateVisibility(true);
+	
+		 
+	     //load slide menu items
+		
+	    map = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+	
+	    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	    getSupportActionBar().setTitle("Seleccione un punto del mapa");
+	    
+		map.setMyLocationEnabled(true);
+
+		map.setOnMapClickListener(new OnMapClickListener() {
+			@Override
+			public void onMapClick(LatLng point) {
+				
+				Projection proj = map.getProjection();
+				Point coord = proj.toScreenLocation(point);
+				
+				
+		util.showToast(Mapv2.this, "Click\n" + "Lat: " + point.latitude + "\n" + "Lng: "
+						+ point.longitude + "\n" + "X: " + coord.x
+						+ " - Y: " + coord.y);
+				
+		
+		///establecemos el paso a la siguiente pantalla y le pasamos valores:
+		/*Intent insert = new Intent(context, InsertCommerce.class);
+		insert.putExtra("latiMain", point.latitude);
+		insert.putExtra("longiMain", point.longitude);
+		enter = 1;
+		insert.putExtra("enter", enter);
+		startActivity(insert);*/
+			}
+		});
+
+		
+		viewPort();	
+		map.setOnCameraChangeListener(new OnCameraChangeListener() {
+			@Override
+			public void onCameraChange(CameraPosition position) {
+
+				LatLngBounds curScreen = map.getProjection().getVisibleRegion().latLngBounds;
+				latitudeNE = curScreen.northeast.latitude;
+				latitudeSW = curScreen.southwest.latitude;
+				longitudeNE = curScreen.northeast.longitude;
+				longitudeSW = curScreen.southwest.longitude;
+				util.log(String.valueOf(latitudeNE));
+				util.log("screen has been recharged");
+				 
+			}
+		});	
+		setSupportProgressBarIndeterminateVisibility(false);
+	}
+	
+	
+	//contains info about the viewposition, clientposition...:
+	public void viewPort(){
+		
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		
+		locationManager.requestLocationUpdates(
+				locationManager.getBestProvider(criteria, false), 0, 0,
+				listener);
+		
+		location = locationManager
+				.getLastKnownLocation(locationManager.getBestProvider(criteria,
+						false));
+		
+		if (location != null) {
+			map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+					location.getLatitude(), location.getLongitude()), 15.0F));
+		
+				
+			CameraPosition cameraPosition = new CameraPosition.Builder()
+					.target(new LatLng(location.getLatitude(), location
+							.getLongitude())) // Sets the center of the map to
+												// location user
+					.zoom(15.0F) // Sets the zoom
+					.build(); //Creates a CameraPosition from the builder
+			map.animateCamera(CameraUpdateFactory
+					.newCameraPosition(cameraPosition));
+			
+			util.log("location"+String.valueOf(location.getLatitude()));
+		}else
+		{
+			util.showToast(context, "location null");
+		}
+		
+	}
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        	case android.R.id.home: 
+        		this.finish();
+        		return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	private LocationListener listener = new LocationListener() {
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+
+		@Override
+		public void onLocationChanged(Location locationn) {
+			double lat = location.getLatitude();
+			double lon = location.getLongitude();
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.putString("latpos", String.valueOf(lat)); 
+			editor.putString("longpos", String.valueOf(lon));
+			editor.commit();
+			
+			locationManager.removeUpdates(listener);	
+		}
+	};
 }
