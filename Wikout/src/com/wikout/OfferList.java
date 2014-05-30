3f276322dac9531776724c504237c88afe7afdc9
Turo <artajahuerce@gmail.com>
2014-05-30 15:01:23 +0200

    15.01

--- Wikout/src/com/wikout/OfferList.java
+++ Wikout/src/com/wikout/OfferList.java
@@ -396,7 +396,7 @@ protected Date actualDate(){
 	public boolean onCreateOptionsMenu(Menu menu) {
 	    // Inflate the menu items for use in the action bar
 	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.menu_refresh, menu);
+	    inflater.inflate(R.menu.main, menu);
 	    return super.onCreateOptionsMenu(menu);
 	}
 }
