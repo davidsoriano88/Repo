635e91383b0d34f8a295c0cd82a1236368997753
davidsoriano88 <davidsorianodc@hotmail.es>
2014-06-04 09:41:39 +0200

    9:41

--- Wikout/src/com/wikout/OfferList.java
+++ Wikout/src/com/wikout/OfferList.java
@@ -114,7 +114,7 @@ public class OfferList extends ActionBarActivity {
 					SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
 					String formatted = format1.format(offer.getDay("deadline").getTime());
 			       holder.tvDeadline.setText("Hasta: " + formatted);
-			       if(offer.getString("description").length()>60){
+			       if(offer.getString("description").length()>55){
 			    	   holder.tvOffer.setText(offer.getString("description").substring(0, 59)+"...");
 			       }else{
 					holder.tvOffer.setText(offer.getString("description"));
